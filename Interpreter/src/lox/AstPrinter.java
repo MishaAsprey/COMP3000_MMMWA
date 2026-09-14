38b9dd6aec64f19f44e42510ab0885cda02b4222
WilliamMilne48231118 <william.milne@students.mq.edu.au>
2026-09-14 14:59:27 +1000

    Added missing astprinter imports

--- Interpreter/src/lox/AstPrinter.java
+++ Interpreter/src/lox/AstPrinter.java
@@ -1,5 +1,8 @@
 package lox;
 
+import lox.Expr.Assign;
+import lox.Expr.Variable;
+
 class AstPrinter implements Expr.Visitor<String> {
   String print(Expr expr) {
     return expr.accept(this);
@@ -50,5 +53,17 @@ class AstPrinter implements Expr.Visitor<String> {
             new Expr.Literal(45.67)));
 
     System.out.println(new AstPrinter().print(expression));
+  }
+
+  @Override
+  public String visitAssignExpr(Assign expr) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'visitAssignExpr'");
+  }
+
+  @Override
+  public String visitVariableExpr(Variable expr) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'visitVariableExpr'");
   }
 }
